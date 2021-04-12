0d4fbb3c932e7631b190589338cdb5a6ddc2f0c0
liufl <602182699@qq.com>
2021-04-12 21:55:06 +0800

    add: 前端基本页面

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const cfg = require('./config')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var staticHandler = express.static('./public', { index: ['index.html'] });
+app.use('/', staticHandler);
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(cfg.port, () => {
   console.log(`my app listening at http://localhost:${cfg.port}`)
